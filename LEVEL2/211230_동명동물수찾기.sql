713f253cd4a10b1dfc483ee36e556df62df80aba
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-12-30 14:09:36 +0900

    동명 동물 수 찾기 문제 해결. 처음에 이중 SELECT문을 이용해서 문제를 해결하였지만, 그냥 HAVING에서 조건을 AND로 묶어서도 해결 가능했음. 오늘 컬럼명 지정해주는 것에 있어서 중요성을 조금 알았는데, 특히 여러 조건문을 사용하거나 이중 SELECT문을 사용할 때, 컬럼명 지정+테이블명 지정이 반드시 필요하다는 것을 알았음

---
+++ LEVEL2/211230_동명동물수찾기.sql
@@ -0,0 +1,12 @@
+-- 간단하게 이렇게 문제를 해결할 수 도 있음.
+SELECT NAME, COUNT(NAME) AS 'COUNT' 
+FROM ANIMAL_INS GROUP BY NAME HAVING (NAME IS NOT NULL AND COUNT >=2);
+
+-- 이중 SELECT문을 이용해서 문제를 해결하였음.
+-- 이중 SELECT문을 어디에 쓰는지에 따라 사용 방법이 달라지는데 보통 where문에서 사용하면 IN함수와 함께 쓰이고,
+-- 여기서 사용한 FROM에서 쓰일 때는 조금 다름. FROM절에는 테이블명이 쓰이도록 되어있음. 따라서 SELECT문을 쓰고 난 후에, B처럼 테이블 명을 지정해줘야함.
+-- 또한 여기서 'COUNT' 컬럼을 명명해줬는데 그걸 하지 않으면 테이블 명이 이상하게 나와서 WHERE절에서 사용하기가 힘듬.
+SELECT *
+FROM (SELECT A.NAME, COUNT(A.NAME) AS 'COUNT' FROM ANIMAL_INS A GROUP BY A.NAME HAVING A.NAME IS NOT NULL) B
+WHERE B.COUNT >= 2
+ORDER BY NAME ASC;
